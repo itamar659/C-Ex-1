527503bda20f72a29c4ea334842ba4161eb8ff05
Yaniv Avrahami <avrahamiyaniv@gmail.com>
2021-03-21 23:55:51 +0200

    changed nothing

---
+++ B21 Ex01 YanivAvrahami 302374921 ItamarAyalon 206024796/B21_Ex01_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B21_Ex01_1
+{
+     class Program
+     {
+          public static void Main() 
+          {
+               Console.WriteLine("Hello!");
+          }
+     }
+}
