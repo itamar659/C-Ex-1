2f30e6f93fab57597d77637acaae7f976816c5f2
Yaniv Avrahami <avrahamiyaniv@gmail.com>
2021-03-22 01:03:02 +0200

    Changed BinaryNumber

--- B21 Ex01 YanivAvrahami 302374921 ItamarAyalon 206024796/B21_Ex01_1/BinaryNumber.cs
+++ B21 Ex01 YanivAvrahami 302374921 ItamarAyalon 206024796/B21_Ex01_1/BinaryNumber.cs
@@ -1,15 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace B21_Ex01_1
 {
-     class BinaryNumber
+     public class BinaryNumber
      {
-          private int m_Length = 7;
-        public string GetNumberOfZeroesOnes()
-        {
-            return string.Empty;
-        }
+          private string m_StrBinaryNum;
+
+          BinaryNumber(string strBinaryNum) 
+          {
+               m_StrBinaryNum = strBinaryNum;
+          }
+          
+          public int GetNumberOfZeros()
+          {
+               int numOfZeros = 0;
+
+               for (int i = 0; i < m_StrBinaryNum.Length; i++)
+               {
+                    if (m_StrBinaryNum[i] == '0')
+                    {
+                         numOfZeros++;
+                    }
+               }
+
+               return numOfZeros;
+          }
+
+          public int GetNumberOfOnes()
+          {
+               int numOfOnes = 0;
+
+               for (int i = 0; i < m_StrBinaryNum.Length; i++)
+               {
+                    if (m_StrBinaryNum[i] == '1')
+                    {
+                         numOfOnes++;
+                    }
+               }
+
+               return numOfOnes;
+          }
+
+         /* public bool IsPowerOfTwo() 
+          {
+               bool seenOneSetDigit = false;
+
+               for (int i = 0; i < m_StrBinaryNum.Length; i++)
+               {
+                    if (m_StrBinaryNum[i] == '1')
+                    {
+                         if (seenOneSetDigit)
+                         {
+                              
+                         }
+                         seenOneSetDigit = true;
+                    }
+
+
+                    
+               }
+          }
+*/
+          public static BinaryNumber Parse(string i_Str)
+          {
+               return new BinaryNumber(i_Str);
+          }
+
+          public static bool TryParse(string i_Str, out BinaryNumber o_BinaryNumber)
+          {
+               bool isParseable;
+
+               if (isBinaryNumber(i_Str))
+               {
+                    o_BinaryNumber = new BinaryNumber(i_Str);
+                    isParseable = true;
+               }
+               else
+               {
+                    o_BinaryNumber = null;
+                    isParseable = false;
+               }
+
+               return isParseable;
+          }
+
+          private static bool isBinaryNumber(string i_Str)
+          {
+               bool res;
+
+               if (isOnlyOnesAndZeros(i_Str))
+               {
+                    res = true;
+               }
+               else
+               {
+                    res = false;
+               }
+
+               return res;
+          }
+
+          private static bool isOnlyOnesAndZeros(string i_Str)
+          {
+               bool res = true;
+
+               foreach (char ch in i_Str)
+               {
+                    if (ch == '0' || ch == '1')
+                    {
+                         continue;
+                    }
+
+                    res = false;
+                    break;
+               }
+
+               return res;
+          }
      }
 }
